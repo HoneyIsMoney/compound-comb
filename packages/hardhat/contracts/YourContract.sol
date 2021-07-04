f1866de82abc3f81a1e7bdf3d995cbfaf59ea41b
honeyismoney <honeyismoney@protonmail.com>
2021-07-04 22:40:44 +0400

    forked xDAI

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,30 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-import "./interfaces/IHoneyFarm.sol"
-import "./interfaces/IUniswapv2Router02.sol"
-import "./interfaces/IERC20.sol"
+import "./interfaces/IHoneyFarm.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IERC20.sol";
 
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
 
   IHoneyFarm public HoneyFarm;
-  IUniswapv2Router02 public UniRouter;
+  IUniswapV2Router02 public UniRouter;
   IERC20 public CombToken;
   IERC20 public PairToken;
 
   event SetPurpose(address sender, string purpose);
 
-  constructor(address _farm, address _router) {
-    HoneyFarms = IHoneyFarm(_farm);
-    UniRouter = IUniswapv2Router02(_router);
+  constructor(
+    address _farm,
+    address _router, 
+    address _comb, 
+    address _pair
+  ) {
+    HoneyFarm = IHoneyFarm(_farm);
+    UniRouter = IUniswapV2Router02(_router);
     CombToken = IERC20(_comb);
     PairToken = IERC20(_pair);
   }
-
 }
--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -3,10 +3,17 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  // XDAI STUFF
+  const router = "0x1C232F01118CB8B424793ae03F870aa7D0ac7f77"
+  const farm = "0xB44825cF0d8D4dD552f2434056c41582415AaAa1"
+  const comb = "0x38Fb649Ad3d6BA1113Be5F57B927053E97fC5bF7"
+  const pair = "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"
+
   await deploy("YourContract", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: ["Hello"],
+    args: [farm, router, comb, pair],
     log: true,
   });
 
